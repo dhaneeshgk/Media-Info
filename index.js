51636cea22503693ac03f8c562bfe5f04e765c8d
Dhaneesh.GK <dhaneeshatservices@gmail.com>
2018-05-24 19:26:40 +0530

    ipc interface

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const electron = require('electron');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
-const { ffmpeg } = require('fluent-ffmpeg')
+const ffmpeg = require('fluent-ffmpeg')
+
+let mainWindow;
 
 app.on('ready', () => {
     console.log("App is now ready");
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
@@ -14,6 +16,7 @@ ipcMain.on('video:submit', (event, path) =>{
 
     ffmpeg.ffprobe(path, (err, metadata) =>{
         console.log('Video Duartion : ',metadata.format.duration);
+        mainWindow.webContents.send('video:duration',metadata.format.duration);
     });
 
 });
